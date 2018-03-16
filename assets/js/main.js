b133224f80e6dbd618c98ad2a2bce0ace98dfcaf
Patricia Georgescu <georgescu.patry@gmail.com>
2018-03-16 17:37:52 +0200

    Sort the entries accordingly

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,67 @@ $(document).ready(function() {
     
     let tasks = JSON.parse(localStorage.getItem("tasks"));
     
+    function sortGeneral(a, b) {
+        return (a < b) ? -1 : (a > b) ? 1 : 0;
+        //return a - b; // does not work with strings!!!
+    }
+    
+    function displayTasks() {
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        if (tasks !== null) {
+            
+            tasks.sort((a,b) => {
+                let aDateClean = a.taskDate.replace(/-/g, "");
+                let bDateClean = b.taskDate.replace(/-/g, "");
+                let aDate = parseInt(aDateClean);
+                let bDate = parseInt(bDateClean);
+                let aTimeClean = a.taskTime.replace(/:/g, "");
+                let bTimeClean = b.taskTime.replace(/:/g, "");
+                let aTime = parseInt(aTimeClean);
+                let bTime = parseInt(bTimeClean);
+                let aPriority = (a.taskPriority === "low") ? 3 : (a.taskPriority === "normal") ? 2 : 1;
+                let bPriority = (b.taskPriority === "low") ? 3 : (b.taskPriority === "normal") ? 2 : 1;
+                let aName = a.task.toLowerCase();
+                let bName = b.task.toLowerCase();
+                //debugger;
+                return sortGeneral(aDate, bDate) || sortGeneral(aTime, bTime) || sortGeneral(aPriority, bPriority) || sortGeneral(aName, bName);
+            });
+             
+            /* tasks.sort((a,b) => {
+                let aDateClean = a.taskDate.replace(/-/g, "");
+                let bDateClean = b.taskDate.replace(/-/g, "");
+                let aDate = parseInt(aDateClean);
+                let bDate = parseInt(bDateClean);
+                let aTimeClean = a.taskTime.replace(/:/g, "");
+                let bTimeClean = b.taskTime.replace(/:/g, "");
+                let aTime = parseInt(aTimeClean);
+                let bTime = parseInt(bTimeClean);
+                let aName = a.task.toLowerCase();
+                let bName = b.task.toLowerCase();
+                if (aDate === bDate) {
+                    if (aTime < bTime) return -1; 
+                    if (aTime > bTime) return 1;
+                    if (aTime === bTime) {
+                        return (aName < bName) ? -1 : (aName > bName) ? 1 : 0;
+                    }
+                }
+                return aDate - bDate;
+            });  */
+             
+            $.each(tasks, function(index, elem) {
+                $(".table").eq(0).append(`<tr id="${elem.id}">
+                                       <td>${elem.task} </td>
+                                       <td>${elem.taskPriority} </td>
+                                       <td>${elem.taskDate} </td>
+                                       <td>${elem.taskTime} </td>
+                                       <td><a href="edit.html?id='${elem.id}'">Edit</a> / <a href="#"> Remove</a></td>
+                                       </tr>`);
+            });
+            
+        }
+    }
+    
     function addTask(event) {
-        //event.preventDefault();
         let date = new Date();
         let year = date.getFullYear();
         let month = date.getMonth() + 1;
@@ -19,7 +78,7 @@ $(document).ready(function() {
         let task = $("#task").val().trim();
         let taskPriority = $("#priority").val();
         let taskDate = $("#date").val(); //"2018-03-20"
-        let dateCleaned = taskDate.replace(/[-]/g, "");
+        let dateCleaned = taskDate.replace(/-/g, "");
         let dateSet = parseInt(dateCleaned);
         let taskTime = $("#time").val(); // "01:00" / "13:00" 
         let timeCleaned = taskTime.split(":").join("");
@@ -49,27 +108,6 @@ $(document).ready(function() {
             tasks.push(newTask);
             localStorage.setItem("tasks", JSON.stringify(tasks));
         } 
-    }
-    
-    function displayTasks() {
-        let tasks = JSON.parse(localStorage.getItem("tasks"));
-        if (tasks !== null) {
-            //tasks.sort(sortAllTasks);
-            $.each(tasks, function(index, elem) {
-                $(".table").eq(0).append(`<tr id="${elem.id}">
-                                       <td>${elem.task} </td>
-                                       <td>${elem.taskPriority} </td>
-                                       <td>${elem.taskDate} </td>
-                                       <td>${elem.taskTime} </td>
-                                       <td><a href="edit.html?id='${elem.id}'">Edit</a> / <a href="#"> Remove</a></td>
-                                       </tr>`);
-            });
-            
-        }
-    }
-    
-    function sortAllTasks() {
-        
     }
     
     displayTasks();
